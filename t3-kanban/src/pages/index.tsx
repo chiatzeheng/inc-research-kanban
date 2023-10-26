53e0fcffea1fec5e15ffca833afc7b730ffb54be
Jocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2023-10-26 10:35:12 +0800

    IGNORE THIS

--- t3-kanban/src/pages/index.tsx
+++ t3-kanban/src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          {/* <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             CREATE T3 KANBAN YEAAA
             <div className=" flex justify-center">
               <img
@@ -23,7 +23,7 @@ export default function Home() {
                 alt="surprise bitches"
               />
             </div>
-          </h1>
+          </h1> */}
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
